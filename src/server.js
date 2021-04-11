ed6ffea529dc3bf645f05037b2ed289294ceafb0
lucas <lucasmengue96@hotmail.com>
2021-04-11 16:15:42 -0300

    :package: commit from day 02 at maratona discovery

--- src/server.js
+++ src/server.js
@@ -2,9 +2,16 @@ const express = require("express");
 const server = express();
 const routes = require("./routes");
 
+// usar template engine
 server.set("view engine", "ejs");
 
+// habilitar arquivos estaticos
 server.use(express.static("public"));
+
+// usar o req.body
+server.use(express.urlencoded({ extended: true }));
+
+// routes
 server.use(routes);
 
 server.listen(3000, () => console.log("Rodando"));
