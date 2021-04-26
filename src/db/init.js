d2ec55b74c5e36f45b75c31aa4c18bdf41c0b715
lucas <lucasmengue96@hotmail.com>
2021-04-26 16:33:46 -0300

    :package: commit from day 04.1 at maratona discovery

--- src/db/init.js
+++ src/db/init.js
@@ -6,11 +6,59 @@ Database.exec(`CREATE TABLE profile (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     avatar TEXT,
-    monthly-budget INT,
-    days-per-week INT,
-    hours-per-day INT,
-    vacation-per-year INT,
-    value-hour INT
+    monthly_budget INT,
+    days_per_week INT,
+    hours_per_day INT,
+    vacation_per_year INT,
+    value_hour INT
+)`);
+
+Database.exec(`CREATE TABLE jobs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    daily_hours INT,
+    total_hours INT,
+    created_at DATETIME
+)`);
+
+Database.run(`INSERT INTO profile (
+    name,
+    avatar,
+    monthly_budget,
+    days_per_week,
+    hours_per_day,
+    vacation_per_year
+) VALUES (
+    "Lucas",
+    "https://avatars.githubusercontent.com/u/31868247?v=4",
+    3000,
+    5,
+    5,
+    4
+)`);
+
+Database.run(`INSERT INTO jobs (
+    name,
+    daily_hours,
+    total_hours,
+    created_at
+) VALUES (
+    "Pizzaria Guloso",
+    2,
+    1,
+    1617514376018
+)`);
+
+Database.run(`INSERT INTO jobs (
+    name,
+    daily_hours,
+    total_hours,
+    created_at
+) VALUES (
+    "OneTwo Project",
+    3,
+    47,
+    1617514376018
 )`);
 
 Database.close();
