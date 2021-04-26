e7c0cf601393afb448aea110a53883b6ecb8a382
lucas <lucasmengue96@hotmail.com>
2021-04-26 15:51:16 -0300

    :package: commit from day 04 at maratona discovery

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -12,7 +12,7 @@ module.exports = {
     // req.body = {name: 'asdf', 'daily-hours': '3.1', 'total-hours': '3'}
     const lastId = jobs[jobs.length - 1] ? jobs[jobs.length - 1].id : 0;
 
-    jobs.push({
+    Job.create({
       id: lastId + 1,
       name: req.body.name,
       "daily-hours": req.body["daily-hours"],
